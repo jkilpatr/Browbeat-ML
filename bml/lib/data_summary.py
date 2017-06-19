3fb3a40a12ac9b5ddf87908fc676c2ea767f0c98
agopi <agopi@redhat.com>
2017-06-19 14:27:24 -0400

    Fixed cloud concurrency issue

--- bml/lib/data_summary.py
+++ bml/lib/data_summary.py
@@ -69,7 +69,11 @@ def print_run_details(config, es_backend, uuid):
         average_runtime=statistics_uuid[0]
         output_string += test_name.ljust(padding) + \
             " " + str(average_runtime) +" "+ str(statistics_uuid[1])  #typecasting data_summary(data) to string because if it returns false it's going to throw an error
-        if float(average_runtime)>0.0 and test_run.errortype=="result" and test_name!="nova.boot_server" and test_run.cloud_name in config['clouds']:
+        if float(average_runtime)>0.0 and test_run.errortype=="result" and \
+           test_name!="nova.boot_server" and \
+           test_run.cloud_name in config['clouds'] and \
+           'pipeline' not in test_run.dlrn_hash and \
+           'trunk' not in test_run.dlrn_hash:
             if str(test_name) in config['test_with_scenario_list']:
                 test_name=str(test_run.scenario_name)+"."+str(test_name)
             output_prediction=classify_value(config,average_runtime,test_name,osp_version)
