f2bdc0c455dcf610c09d1bd17f65072d231dfdec
agopi <agopi@redhat.com>
2017-06-28 09:41:33 -0400

    debugging

--- bml/lib/dtree_classifier.py
+++ bml/lib/dtree_classifier.py
@@ -1,7 +1,6 @@
 import numpy
 import cPickle
 
-
 def classify_value(config, value, test_name, osp_version):
     predictors = numpy.array([0, 1, 2])
     osp_version_dic = reduce(lambda r, d: r.update(d) or r, config['osp_version_dic'], {})  # noqa
@@ -10,7 +9,8 @@ def classify_value(config, value, test_name, osp_version):
     predictors[1] = test_name_dic[str(test_name)]
     predictors[2] = float(value)
     predictors.reshape(1, -1)
-    with open('dumped_dtree.pkl', 'rb') as fid:
-        clf = cPickle.load(fid)
+    subprocess.call(["pwd"])
+    #with open('dumped_dtree.pkl', 'rb') as fid:
+    #    clf = cPickle.load(fid)
     output_prediction = clf.predict([predictors])
     return output_prediction
