167ccf0b464aa42bb9858ba599a78c4a16216af6
agopi <agopi@redhat.com>
2017-07-07 14:07:08 -0400

    Everything working fine

--- bml/lib/data_summary.py
+++ bml/lib/data_summary.py
@@ -5,10 +5,10 @@ import psycopg2
 
 
 def insert_values_db(config, uuid, test, osp_name, avg_runtime, grade):
-    db_name = config['database']
-    user_name = config['user_name']
-    host_ip = config['host']
-    conn = psycopg2.connect(database=db_name, user=user_name, host=host_ip, port=26257)  # noqa
+    db_name = config['database'][0]
+    user_name = config['user_name'][0]
+    host_ip = config['host'][0]
+    conn = psycopg2.connect(database=db_name, user=user_name, host=str(host_ip), port=26257)  # noqa
     conn.set_session(autocommit=True)
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS grades (uuid STRING, test STRING, osp_name STRING, avg_runtime FLOAT, grade INT)")  # noqa
