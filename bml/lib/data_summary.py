221b587aa6981cb250c5202499c79d188b518b71
agopi <agopi@redhat.com>
2017-08-11 17:12:27 -0400

    looks at errors, also stores 95th perc scores
    1. Browbeat-ML now looks at the total number of errors and fails if more than 1000 errors
    
    2. We now store 95th percentile of the raw values but the classifier doesn't use the metric
    
    3. Store the number of errors in the table table_errors in config.yaml

--- bml/lib/data_summary.py
+++ bml/lib/data_summary.py
@@ -3,6 +3,7 @@ from perf_classifier import classify_value
 import numpy
 from update_crdb import insert_values_db
 from update_crdb import insert_grades_db
+from update_crdb import insert_errors_db
 from util import longest_test_name
 
 
@@ -35,13 +36,15 @@ def summary_uuid(es_backend, config, uuid, update):
         print(val)
         if "FAIL" in str(val):
             exit(1)
+        print("Found no problems with the uuid")
 
 
 def data_summary(data):
     std_dev = "{:.4f}".format(numpy.std(data)).ljust(10)
     avg = "{:.4f}".format(numpy.mean(data)).ljust(10)
     median = "{:.4f}".format(numpy.median(data)).ljust(10)
-    summary = [avg, std_dev, median]
+    percentile95 = "{:.4f}".format(numpy.percentile(data, 95)).ljust(10)
+    summary = [avg, std_dev, median, percentile95]
     return(summary)
 
 
@@ -51,6 +54,7 @@ def print_run_details(config, es_backend, uuid, update):
     output_string = ""
     osp_version = ""
     padding = longest_test_name(config)
+    test_clean_count = 0  # count of the tests that are being classified
     '''
     conn=sqlite3.connect('database.sqlite')
     c=conn.cursor()
@@ -59,6 +63,7 @@ def print_run_details(config, es_backend, uuid, update):
         test_name = test_type['test']
         data = []
         test_run = None
+        check_outcome = 0  # just to see if it passses all conditions
         for test_run in brun.get_tests(test_search=test_name):
             data.extend(test_run.raw)
             osp_version = test_run.version
@@ -66,30 +71,34 @@ def print_run_details(config, es_backend, uuid, update):
             continue
         statistics_uuid = data_summary(data)
         average_runtime = statistics_uuid[0]
+        perc95_score = statistics_uuid[3]
         output_string += test_name.ljust(padding) + \
-            " " + str(average_runtime) + " " + str(statistics_uuid[1])
+            " " + str(average_runtime) + " " + str(statistics_uuid[1]) + " " +\
+            perc95_score
         test_original = test_name
         if str(test_name) in config['test_with_scenario_list']:
             test_name = str(test_run.scenario_name) + "." + str(test_name)
         times = test_run.times
         concurrency = test_run.concurrency
         time_check = float(average_runtime) > 0.0
-        result_check = test_run.errortype == "result"
         test_checks = test_original not in config['ignored_tests']
         cloud_check = test_run.cloud_name in config['clouds']
         dlrn_hash = test_run.dlrn_hash
         puddle = test_run.rhos_puddle
         hash_check = check_hash(dlrn_hash, puddle)
-        if time_check and result_check and cloud_check and hash_check:
+        if time_check and cloud_check and hash_check:
+            check_outcome = 1
             if test_checks:
                 output_prediction = classify_value(config, average_runtime,
                                                    test_name, osp_version,
                                                    times, concurrency)
+                test_clean_count += 1
                 if update:
                     insert_grades_db(config, uuid, test_name, osp_version,
                                      average_runtime, output_prediction,
                                      test_run.timestamp, puddle,
-                                     dlrn_hash, concurrency, times)
+                                     dlrn_hash, concurrency, times,
+                                     perc95_score)
                 if int(output_prediction) == 1:
                     print("ALERT!!!!")
                     print(uuid, test_name, osp_version, average_runtime)
@@ -101,7 +110,8 @@ def print_run_details(config, es_backend, uuid, update):
                 if update:
                     insert_values_db(config, uuid, test_name, osp_version,
                                      average_runtime, test_run.timestamp,
-                                     puddle, dlrn_hash, concurrency, times)
+                                     puddle, dlrn_hash, concurrency, times,
+                                     perc95_score)
                 output_string += "\n"
         else:
             output_string += "\n"
@@ -110,6 +120,16 @@ def print_run_details(config, es_backend, uuid, update):
     conn.close()
     '''
     header = ("Browbeat UUID: " + uuid + " OSP_version: " + osp_version + "\n")
+    if check_outcome == 1:
+        total_errors = brun.get_num_errors()
+        if total_errors > 1000:
+            total_errors = str(total_errors) + " FAIL"
+        else:
+            total_errors = str(total_errors)
+        header = header + ("Errors: " + total_errors + "\n")
+        if test_clean_count >= 13:
+            if update:
+                insert_errors_db(config, uuid, brun.get_num_errors())
     header += ("".ljust(80, "-")) + "\n"
     output_string = header + output_string
     return output_string
